ecb72135eec9e19c5cd7350885f81063254e2402
Mike Potvin <mike@redbitdev.com>
2024-12-13 09:08:21 -0500

    fix: added MapboxNavigationApp setup to module creation

--- android/src/main/java/com/everdriven/mapboxnavigation/MapboxNavigationPackage.kt
+++ android/src/main/java/com/everdriven/mapboxnavigation/MapboxNavigationPackage.kt
@@ -4,9 +4,18 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
+import com.mapbox.navigation.base.options.NavigationOptions
 
 class MapboxNavigationPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    if (!MapboxNavigationApp.isSetup()) {
+      MapboxNavigationApp.setup {
+          NavigationOptions.Builder(reactContext)
+              // additional options
+              .build()
+      }
+    }
     return emptyList()
   }
 
